9c8545bac6446edbe14c61311ff98ad86568c0c6
Mirco Kraenz <kraenzmirco@gmail.com>
2020-06-07 18:05:40 +0900

    ROCKIFY #1 \m/ the calculator \m/

--- src/parse.test.ts
+++ src/parse.test.ts
@@ -17,32 +17,32 @@ const divided = DividedExpression.of;
 const num = TerminalExpression.of;
 
 describe("tokenize and parse", () => {
-    it("works for 5 + 3", () => {
-        const input = "5 + 3";
+    it("works for 5 Rock! 3", () => {
+        const input = "5 Rock! 3";
 
         const result = tokenizeAndParse(input);
 
         expect(result).to.deep.equal(plus(num(5), num(3)));
     });
 
-    it("works for 5 + 3 + 1", () => {
-        const input = "5 + 3 + 1";
+    it("works for 5 Rock! 3 Rock! 1", () => {
+        const input = "5 Rock! 3 Rock! 1";
 
         const result = tokenizeAndParse(input);
 
         expect(result).to.deep.equal(plus(plus(num(5), num(3)), num(1)));
     });
 
-    it("works for 4 * 5 + 3", () => {
-        const input = "4 * 5 + 3";
+    it("works for 4 Yeah! 5 Rock! 3", () => {
+        const input = "4 Yeah! 5 Rock! 3";
 
         const result = tokenizeAndParse(input);
 
         expect(result).to.deep.equal(plus(times(num(4), num(5)), num(3)));
     });
 
-    it("works for 4 * 5 + 3 * 8", () => {
-        const input = "4 * 5 + 3 * 8";
+    it("works for 4 Yeah! 5 Rock! 3 Yeah! 8", () => {
+        const input = "4 Yeah! 5 Rock! 3 Yeah! 8";
 
         const result = tokenizeAndParse(input);
 
@@ -51,24 +51,25 @@ describe("tokenize and parse", () => {
         );
     });
 
-    it("works for ( 4 * 5 )", () => {
-        const input = "( 4 * 5 )";
+    it("works for EVERYBODY_NOW 4 Yeah! 5 JUMP!", () => {
+        const input = "EVERYBODY_NOW 4 Yeah! 5 JUMP!";
 
         const result = tokenizeAndParse(input);
 
         expect(result).to.deep.equal(times(num(4), num(5)));
     });
 
-    it("works for ( 4 * 5 ) + 3", () => {
-        const input = "( 4 * 5 ) + 3";
+    it("works for EVERYBODY_NOW 4 Yeah! 5 JUMP! Rock! 3", () => {
+        const input = "EVERYBODY_NOW 4 Yeah! 5 JUMP! Rock! 3";
 
         const result = tokenizeAndParse(input);
 
         expect(result).to.deep.equal(plus(times(num(4), num(5)), num(3)));
     });
 
-    it("works for ( 4 * 5 ) + ( 2 * 3 )", () => {
-        const input = "( 4 * 5 ) + ( 2 * 3 )";
+    it("works for EVERYBODY_NOW 4 Yeah! 5 JUMP! Rock! EVERYBODY_NOW 2 Yeah! 3 JUMP!", () => {
+        const input =
+            "EVERYBODY_NOW 4 Yeah! 5 JUMP! Rock! EVERYBODY_NOW 2 Yeah! 3 JUMP!";
 
         const result = tokenizeAndParse(input);
 
@@ -77,8 +78,9 @@ describe("tokenize and parse", () => {
         );
     });
 
-    it("works for ( 4 * 5 ) + ( ( 2 * 3 ) * 7 )", () => {
-        const input = "( 4 * 5 ) + ( ( 2 * 3 ) * 7 )";
+    it("works for EVERYBODY_NOW 4 Yeah! 5 JUMP! Rock! EVERYBODY_NOW EVERYBODY_NOW 2 Yeah! 3 JUMP! Yeah! 7 JUMP!", () => {
+        const input =
+            "EVERYBODY_NOW 4 Yeah! 5 JUMP! Rock! EVERYBODY_NOW EVERYBODY_NOW 2 Yeah! 3 JUMP! Yeah! 7 JUMP!";
 
         const result = tokenizeAndParse(input);
 
@@ -87,16 +89,16 @@ describe("tokenize and parse", () => {
         );
     });
 
-    it("works for 4 - 5", () => {
-        const input = "4 - 5";
+    it("works for 4 Shout! 5", () => {
+        const input = "4 Shout! 5";
 
         const result = tokenizeAndParse(input);
 
         expect(result).to.deep.equal(minus(num(4), num(5)));
     });
 
-    it("works for 4 - 5 + 4 - ( 3 * 2 )", () => {
-        const input = "4 - 5 + 4 - ( 3 * 2 )";
+    it("works for 4 Shout! 5 Rock! 4 Shout! EVERYBODY_NOW 3 Yeah! 2 JUMP!", () => {
+        const input = "4 Shout! 5 Rock! 4 Shout! EVERYBODY_NOW 3 Yeah! 2 JUMP!";
 
         const result = tokenizeAndParse(input);
 
@@ -105,16 +107,17 @@ describe("tokenize and parse", () => {
         );
     });
 
-    it("works for 6 / 3", () => {
-        const input = "6 / 3";
+    it("works for 6 Death! 3", () => {
+        const input = "6 Death! 3";
 
         const result = tokenizeAndParse(input);
 
         expect(result).to.deep.equal(divided(num(6), num(3)));
     });
 
-    it("works for 4 - 5 + 4 / ( 18 / 2 )", () => {
-        const input = "4 - 5 + 4 / ( 18 / 2 )";
+    it("works for 4 Shout! 5 Rock! 4 Death! EVERYBODY_NOW 18 Death! 2 JUMP!", () => {
+        const input =
+            "4 Shout! 5 Rock! 4 Death! EVERYBODY_NOW 18 Death! 2 JUMP!";
 
         const result = tokenizeAndParse(input);
 
